53a4909e5890536e2308536e04fadda84a11cc01
Henry Monroy <hmonroy@asadacloud.com>
2018-04-05 11:13:47 -0400

    Mensaje prueba Tortoise

---
+++ curso csharpviedeo28.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videos
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            int numero1 = 4;
+            int numero2 = 8;
+            double promedio = CalcularPromedio(numero1, numero2);
+            Console.Read();    
+        }
+
+        private static double CalcularPromedio(int numero1, int numero2)
+        {
+            return (numero1 + numero2) / 2.0;
+        }
+    }
+}
